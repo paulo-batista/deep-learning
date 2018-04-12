965db579dfa8bb6274238137e3dd270d569ae9c9
Paulo Batista <paulobatista.phd@gmail.com>
2018-04-12 17:01:14 -0300

    Data preprocessing completed
    Data preprocessing completed

--- ANN/ann.py
+++ ANN/ann.py
@@ -26,5 +26,16 @@ X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
 
 onehotencoder = OneHotEncoder(categorical_features= [1])
 X = onehotencoder.fit_transform(X).toarray()
+X = X[:, 1:]
+
+#Spliting dataset training and test
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+# Feature Scaling 
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
 
 
